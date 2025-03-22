77771017ebfc8e913de7562833c9c4fa8cc1bd49
DorsaBeigifard <dorsabeigi@gmail.com>
2025-03-22 15:49:08 +0330

    nearby locations slider

--- src/components/LocationList/LocationList.jsx
+++ src/components/LocationList/LocationList.jsx
@@ -1,36 +1,103 @@
 import useFetch from "../../hooks/useFetch";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
+
+const SlickButtonFix = ({ currentSlide, slideCount, children, ...props }) => (
+  <span {...props}>{children}</span>
+);
 
 function LocationList() {
   const { data, isLoading } = useFetch("http://localhost:5000/hotels", "");
 
-  if (isLoading) <p>Loading... </p>;
+  if (isLoading) return <p>Loading... </p>;
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    nextArrow: (
+      <SlickButtonFix>
+        <ChevronRightIcon className="h-8 w-8 text-blue-900 hidden lg:block" />
+      </SlickButtonFix>
+    ),
+    prevArrow: (
+      <SlickButtonFix>
+        <ChevronLeftIcon className="h-8 w-8 text-blue-900 hidden lg:block" />
+      </SlickButtonFix>
+    ),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 0,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 0,
+        },
+      },
+    ],
+  };
 
   return (
-    <div className="container mx-auto section-spacing">
-      <div className="nearbyLocations">
-        <h2>Nearby Locations </h2>
-        <div className="grid grid-flow-col grid-rows-2">
-          {data.map((item) => {
-            return (
-              <div
-                className="locationItem rounded-xl overflow-hidden bg-white"
-                key={item.id}
-              >
-                <img
-                  src={item.thumbnail_url}
-                  alt={item.name}
-                  className="w-full h-50 object-cover"
-                />
-                <div className="locationItemDesc">
-                  <p className="location">{item.smart_location}</p>
-                  <p className="name">{item.name}</p>
-                  <p className="price">
-                    €&nbsp;{item.price}&nbsp;<span>night</span>
-                  </p>
+    <div className="bg-gray-50 overflow-hidden">
+      <div className="container mx-auto section-spacing ">
+        <div className="nearbyLocations">
+          <div className="text-center mb-8">
+            <h2>Nearby Locations </h2>
+            <p>
+              Find top-rated hotels near you for a comfortable and convenient
+              stay.
+            </p>
+          </div>
+          <div>
+            <Slider {...settings}>
+              {data.map((item) => (
+                <div
+                  className="locationItem rounded-xl overflow-hidden bg-white "
+                  key={item.id}
+                >
+                  <img
+                    src={item.thumbnail_url}
+                    alt={item.name}
+                    className="w-full h-50 object-cover"
+                  />
+                  <div className="locationItemDesc h-30 lg:h-40 p-4 flex flex-col justify-between">
+                    <div>
+                      <p className="location font-medium">{item.smart_location}</p>
+                      <p className="name mb-4">{item.name}</p>
+                    </div>
+                    <div className="flex justify-between items-center">
+                      <p className="price font-bold">
+                        €&nbsp;{item.price}&nbsp;<span>per night</span>
+                      </p>
+                      <button className="text-blue-700">See Details</button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              ))}
+            </Slider>
+          </div>
         </div>
       </div>
     </div>
