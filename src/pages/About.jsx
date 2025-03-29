6b0f42b2a5b515ae56cb2e71c8549749248e9c22
DorsaBeigifard <dorsabeigi@gmail.com>
2025-03-29 16:54:10 +0330

    v1

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="flex items-center justify-center h-[80vh] text-center">
+      You can develop this section later to provide more details about your
+      company, mission, and values. <br />For now, this placeholder ensures everything
+      is in place for future content updates.
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="flex items-center justify-center h-[80vh] text-center">
+      You can develop this section later to provide more details about your
+      company, mission, and values. <br />For now, this placeholder ensures everything
+      is in place for future content updates.
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
